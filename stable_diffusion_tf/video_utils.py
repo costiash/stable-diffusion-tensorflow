85f5ae3c34b512eb673d39b764e738332c00c567
Constantin Shafranski <63783894+costiash@users.noreply.github.com>
2022-11-12 01:57:52 +0200

    Update video_utils.py

--- stable_diffusion_tf/video_utils.py
+++ stable_diffusion_tf/video_utils.py
@@ -105,7 +105,7 @@ def create_prompts_frames_dict(first_prompt, first_frame=0, second_prompt=None, 
     if not first_prompt:
         raise RuntimeError('Please make sure you passed a valid prompt')
     
-    if int(first_frame) != 0:
+    if first_frame != 0:
         raise RuntimeError('The number of the first frame must to be 0')
 
     prompts_frames_dict['prompt1'] = [first_prompt, first_frame]
@@ -219,7 +219,6 @@ def generate_init_frame(curr_prompt, args, generator):
         temperature=1,
         batch_size=1,
     )
-
     next_seed(args)
     return img[0]
 
@@ -246,7 +245,12 @@ def construct_ffmpeg_video_cmd(args, frames_path, mp4_path):
         mp4_path
     ]
 
-    return cmd
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    
+    if process.returncode != 0:
+        print(stderr)
+        raise RuntimeError(stderr)
 
 
 def create_audio(args, sound_path, mp3_path):
@@ -259,6 +263,7 @@ def create_audio(args, sound_path, mp3_path):
 
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
+    
     if process.returncode != 0:
         print(stderr)
         raise RuntimeError(stderr)
@@ -276,4 +281,9 @@ def construct_ffmpeg_combined_cmd(vid_path, aud_path, combined_path):
         combined_path
     ]
 
-    return cmd
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    
+    if process.returncode != 0:
+        print(stderr)
+        raise RuntimeError(stderr)
